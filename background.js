808a013c60bd4e246bad3027ebfd3d0144a7f12c
Sonywahyupratama <142053324+Sonywahyupratama@users.noreply.github.com>
2023-08-26 21:45:55 +0700

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
-chrome.webRequest.onBeforeRequest.addListener(
+chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
-    if (details.method === "POST" && details.url === "https://quack.duckduckgo.com/api/email/addresses") {
-      // Modify request headers here if needed
-    }
+    var headers = details.requestHeaders;
+    headers.push(
+      { name: "Authorization", value: "Bearer tkqyrvpmclswd0tm7i6qquwsokjibf6owfg6udvdgvrnq6jip9wurnpk7rupbo" },
+      { name: "User-Agent", value: "ddg_android/5.166.0 (com.duckduckgo.mobile.android; Android API 29)" },
+      { name: "Content-Length", value: "0" },
+      { name: "Accept-Encoding", value: "gzip" }
+    );
+    return { requestHeaders: headers };
   },
   { urls: ["https://quack.duckduckgo.com/*"] },
-  ["blocking"]
+  ["blocking", "requestHeaders"]
 );
