c0964ea52bb2f72bdfe73fe91163786558238e02
Sonywahyupratama <142053324+Sonywahyupratama@users.noreply.github.com>
2023-08-26 21:46:54 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const modifyButton = document.getElementById("modifyButton");
-  const resultDiv = document.getElementById("result");
-
-  modifyButton.addEventListener("click", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      const activeTab = tabs[0];
-      chrome.scripting.executeScript({
-        target: { tabId: activeTab.id },
-        function: modifyEmail,
-        args: [{ action: "modifyEmail", address: "e1wrjuor" }]
-      });
-    });
-  });
-
-  function modifyEmail(params) {
-    chrome.runtime.sendMessage(params, function(response) {
-      resultDiv.textContent = "New email address: " + response.newAddress;
-    });
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.email) {
+      document.getElementById("email").textContent = request.email;
+    }
   }
-});
+);
