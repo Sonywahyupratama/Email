7e83f6c04874260a00eb6665a56fa79ba823c676
Sonywahyupratama <142053324+Sonywahyupratama@users.noreply.github.com>
2023-08-26 21:26:52 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,22 +1,21 @@
-fetch("https://quack.duckduckgo.com/api/email/addresses", {
-  method: "POST",
-  headers: {
-    "Host": "quack.duckduckgo.com",
-    "Authorization": "Bearer tkqyrvpmclswd0tm7i6qquwsokjibf6owfg6udvdgvrnq6jip9wurnpk7rupbo",
-    "User-Agent": "ddg_android/5.166.0 (com.duckduckgo.mobile.android; Android API 29)",
-    "Content-Length": "0",
-    "Accept-Encoding": "gzip"
+document.addEventListener("DOMContentLoaded", function() {
+  const modifyButton = document.getElementById("modifyButton");
+  const resultDiv = document.getElementById("result");
+
+  modifyButton.addEventListener("click", function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const activeTab = tabs[0];
+      chrome.scripting.executeScript({
+        target: { tabId: activeTab.id },
+        function: modifyEmail,
+        args: [{ action: "modifyEmail", address: "e1wrjuor" }]
+      });
+    });
+  });
+
+  function modifyEmail(params) {
+    chrome.runtime.sendMessage(params, function(response) {
+      resultDiv.textContent = "New email address: " + response.newAddress;
+    });
   }
-})
-.then(response => response.json())
-.then(data => {
-  if (data.address) {
-    var newAddress = `${data.address}@duck.com`;
-    console.log("New email address:", newAddress);
-  } else {
-    console.log("Error: Unable to get new email address");
-  }
-})
-.catch(error => {
-  console.error("Error:", error);
 });
