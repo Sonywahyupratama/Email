40d7600da2dec6bcb83b7695b2d497af65e7763f
Sonywahyupratama <142053324+Sonywahyupratama@users.noreply.github.com>
2023-09-10 03:05:09 +0700

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,11 @@
-chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
-    for (var i = 0; i < details.requestHeaders.length; ++i) {
-      if (details.requestHeaders[i].name === 'Authorization') {
-        details.requestHeaders[i].value = 'Bearer tkqyrvpmclswd0tm7i6qquwsokjibf6owfg6udvdgvrnq6jip9wurnpk7rupbo';
-        break;
-      }
-    }
-    return { requestHeaders: details.requestHeaders };
-  },
-  { urls: ["https://quack.duckduckgo.com/*"] },
-  ["blocking", "requestHeaders"]
-);
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: function () {
+      const url = 'https://www.wts.pw/full/?api=c459ca2d2ff7da18fec24ab01fb49d529ca0dc99&url=aHR0cHM6Ly9sLmhzdi5teS5pZC82Oi9bTEVORFJJVkVdJTIwSW51JTIwbmklMjAoRHVhbCUyMFN1YnMpJTIwKDAxLTEyKSUyMFsxMDgwcF1bSEVWQyUyMDEwYml0XS5yYXI=&type=2';
+      const encoded_url = new URL(url).searchParams.get('url');
+      const decoded_url = atob(encoded_url);
+      alert(decoded_url);
+    },
+  });
+});
